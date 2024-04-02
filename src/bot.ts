5db8195b55368e5d37e1572d367f15234ec8930a
Егор Матяс <play.filin@gmail.com>
2024-04-02 16:23:03 +0300

    feat: added util error handler

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
-import { Bot, GrammyError, HttpError } from 'grammy';
+import { Bot } from 'grammy';
 import schedule from 'node-schedule';
+import errorHandler from './utils/errorHandler';
 
 dotenv.config();
 const bot = new Bot(process.env.BOT_API_KEY!);
@@ -43,18 +44,7 @@ bot.on('message', async ctx => {
   }
 });
 
-bot.catch(err => {
-  const ctx = err.ctx;
-  console.error(`Error while handling update ${ctx.update.update_id}:`);
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error('Error in request:', e.description);
-  } else if (e instanceof HttpError) {
-    console.error('Could not contact Telegram:', e);
-  } else {
-    console.error('Unknown error:', e);
-  }
-});
+bot.catch(err => errorHandler(err));
 
 bot.start();
 // 442877184
