0858433e3cb8eb9d5d33eb83005338bcbd97cc24
Егор Матяс <play.filin@gmail.com>
2024-04-16 13:48:25 +0300

    feat: tested sch

--- build/src/utils/startCommand.js
+++ build/src/utils/startCommand.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const node_schedule_1 = __importDefault(require("node-schedule"));
 const startCommand = async (ctx, bot) => {
     await ctx.reply('Бот гильдии "Вальхалла" приветствует тебя!');
-    node_schedule_1.default.scheduleJob({ hour: 12, minute: 0, dayOfWeek: 1 }, async function () {
+    node_schedule_1.default.scheduleJob({ hour: 13, minute: 50, dayOfWeek: 2 }, async function () {
         if (ctx.chat) {
             await bot.api.sendMessage(ctx.chat.id, '☠️ Не забудьте нанести урон боссам Проклятого мира и Коридора кошмаров. У вас осталось всего три часа на это.');
         }
